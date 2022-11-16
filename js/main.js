7fa8b31fb20559eaf4ee9c4cbeda81c0eb870e40
Dennys-Reys <denilson.reis@sptech.school>
2022-11-15 22:17:34 -0300

    paginas finalizadas, só organizar mais até apresentação

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
-// $('.carrosssel').slick({
-//     dots: true,
-//     infinite: true,
-//     speed:300,
-//     slidesToShow: 1,
-//     // adaptiveHeight: true
+let count = 1;
+document.getElementById("radio1").checked = true;
 
-// });
+setInterval(function(){
+proxImagem();
+},5000)
 
+function proxImagem(){
+    count++;
+    if (count>4) {
+        count =1;
+    }
+    document.getElementById("radio" + count).checked = true;
+}
